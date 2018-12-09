b8f72478f7c573a7aee4ed74391a1577e825c31e
Stef Lewandowski <stef@stef.io>
2018-12-09 18:55:41 +0000

    attempt 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Hero, Layout, Heading } from 'components'
+
+class FourOhFour extends Component {
+  render() {
+    return (
+      <Layout color={'rose'}>
+        <Hero color={'rose'}>
+          <Heading
+            size={1}
+            color="white"
+            bg="black"
+            text="Sorry - Page not found"
+          />
+          <Heading
+            size={3}
+            color="black"
+            bg="white"
+            text="Perhaps try checking out our other pages to find what you are looking for"
+          />
+        </Hero>
+      </Layout>
+    )
+  }
+}
+
+export default FourOhFour
+
+FourOhFour.propTypes = {
+  data: PropTypes.shape({
+    posts: PropTypes.object.isRequired
+  }).isRequired
+}
