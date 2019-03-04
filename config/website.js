1865d010592225c50a77e512acc26ff278ce86fc
Stef Lewandowski <stef@stef.io>
2019-03-04 18:01:31 +0000

    ICE list description for SEO

--- config/website.js
+++ config/website.js
@@ -1,9 +1,9 @@
 module.exports = {
   _pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  _title: 'ICE', // Navigation and Site Title
-  _titleAlt: 'ICE', // Title for JSONLD
-  description: 'ICE',
-  _url: 'https://icelist.eu', // Domain of your site. No trailing slash!
+  _title: 'The ICE List', // Navigation and Site Title
+  _titleAlt: 'The ICE List', // Title for JSONLD
+  description: 'We’ve built a community of the world’s most exciting entrepreneurs. Want to join the adventure?',
+  _url: 'https://theicelist.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   logo: '/logos/ice-logo@2x.png', // Used for SEO
 
