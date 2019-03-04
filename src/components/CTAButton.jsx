036c36aa7d622df870c94b657ae26c22a04e6840
Stef Lewandowski <stef@stef.io>
2019-03-04 09:55:01 +0000

    cta button

---
+++ src/components/CTAButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'react-emotion'
+
+const Button = styled.button`
+  text-decoration: none;
+  border-bottom: 4px solid white;
+  ${tw`bg-black text-white px-3 py-2 m-auto text-center`};
+`
+
+class CTAButton extends React.Component {
+  render() {
+    const { href, text } = this.props
+    return <Button href={href}>{text}</Button>
+  }
+}
+
+export default CTAButton
