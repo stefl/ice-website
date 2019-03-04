4cee7535393008b32632675ca6beb697a086936c
Stef Lewandowski <stef@stef.io>
2019-03-04 18:12:44 +0000

    test production bundle

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,6 +5,9 @@ import { Hero, Layout, Heading } from 'components'
 class FourOhFour extends Component {
   render() {
     return (
+      <div>
+        <div>Testing</div>
+
       <Layout color="rose">
         <Hero color="rose">
           <Heading size={1} color="white" bg="black" text="Sorry - Page not found" />
@@ -16,6 +19,7 @@ class FourOhFour extends Component {
           />
         </Hero>
       </Layout>
+      </div>
       
     )
   }
